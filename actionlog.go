eafd4331906f6695b5fd729a6fb3dca52957236d
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2020-02-27 13:24:56 -0700

    stubbed out function and unit test for GetStats

--- actionlog.go
+++ actionlog.go
@@ -1,7 +1,11 @@
 package actionlog
 
-
 //AddAction Adds an action
 func AddAction(actionString string) error {
 	return nil
+}
+
+//GetStats Gets stats for all actions
+func GetStats() string {
+	return ""
 }
--- actionlog_test.go
+++ actionlog_test.go
@@ -10,3 +10,12 @@ func TestAddAction(t *testing.T) {
 		t.Errorf("Adding first action failed, got: %d, want: %d.", actual, expected)
 	}
 }
+
+func TestGetStats(t *testing.T) {
+	var expected string = ""
+	var actual string = GetStats()
+
+	if expected != actual {
+		t.Errorf("Get Stats failed, got: %s, want: %s.", actual, expected)
+	}
+}
