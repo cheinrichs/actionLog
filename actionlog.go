57e78ea44cf1e2cc9c90b999cf63593a68c158d8
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2020-02-27 13:50:27 -0700

    added calculateAverage function and an updateAverage function to fill out functionality for get stats

--- actionlog.go
+++ actionlog.go
@@ -19,6 +19,13 @@ type actionAverage struct {
 //}
 var actionData = make(map[string][]int)
 
+//Stores averages for each action
+// [
+// 	{"Action": Avg},
+// 	{"Action": Avg},
+// ]
+var averages = []actionAverage{}
+
 //AddAction Adds an action
 func AddAction(actionString string) error {
 	bytes := []byte(actionString)
@@ -36,10 +43,49 @@ func AddAction(actionString string) error {
 		actionData[e.Action] = []int{e.Time}
 	}
 
+	average := calculateAverage(e.Action)
+
+	updateAverage(e.Action, average)
+
 	return nil
+}
+
+//calculates the average based on all the times so far
+func calculateAverage(key string) int {
+	var currentAction = actionData[key]
+	var sumOfTimes = 0
+
+	for i := 0; i < len(actionData[key]); i++ {
+		sumOfTimes += currentAction[i]
+	}
+
+	return sumOfTimes / len(currentAction)
+}
+
+//Updates the current average for a specific action
+func updateAverage(action string, avg int) {
+	//Update the average for the specific action
+	for i := 0; i < len(averages); i++ {
+		if averages[i].Action == action {
+			averages[i].Avg = avg
+			return
+		}
+	}
+
+	//If an actionAverage record hasn't been added yet, add it
+	newActionAverage := actionAverage{
+		Action: action,
+		Avg:    avg,
+	}
+	averages = append(averages, newActionAverage)
 }
 
 //GetStats Gets stats for all actions
 func GetStats() string {
-	return ""
+	averagesJSON, err := json.Marshal(averages)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(averagesJSON)
 }
