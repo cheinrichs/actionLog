20caf86d9da558a4c7b9f84bfdb3f4f43d56529a
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2020-02-27 13:41:20 -0700

    added unit tests based on requirements

--- actionlog_test.go
+++ actionlog_test.go
@@ -11,6 +11,24 @@ func TestAddAction(t *testing.T) {
 	}
 }
 
+func TestSecondAddAction(t *testing.T) {
+	var expected error = nil
+	var actual = AddAction("{\"action\":\"run\", \"time\":75}")
+
+	if expected != actual {
+		t.Errorf("Adding second action failed, got: %s, want: %s.", actual, expected)
+	}
+}
+
+func TestThirdAddAction(t *testing.T) {
+	var expected error = nil
+	var actual = AddAction("{\"action\":\"jump\", \"time\":200}")
+
+	if expected != actual {
+		t.Errorf("Adding third action failed, got: %s, want: %s.", actual, expected)
+	}
+}
+
 func TestGetStats(t *testing.T) {
 	var expected string = ""
 	var actual string = GetStats()
